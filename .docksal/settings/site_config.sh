a7da91290184d09f0d27af8edceb08a8ddbace6d
Ignacio Bonelli <ignacio@nachodigital.com.ar>
2022-02-25 17:16:53 -0300

    Adding needed configuration.

---
+++ .docksal/settings/site_config.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+export DRUPAL_INSTALL_ADMIN_NAME="admin"
+export DRUPAL_INSTALL_ADMIN_PASS="admin"
+export DRUPAL_INSTALL_ADMIN_MAIL="admin@admin.com"
+export DRUPAL_INSTALL_SITE_NAME="Drupal WebComponents Sandbox" 
